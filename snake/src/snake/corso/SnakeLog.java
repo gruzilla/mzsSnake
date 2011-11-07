37d07a25ebc394f38e8a076094de901b88c0af9a
Jakob Lahmer <jakob_l@gmx.at>
2011-11-07 19:11:43 +0100

    removed corso dependencies, identified corso connections

--- snake/src/snake/corso/SnakeLog.java
+++ snake/src/snake/corso/SnakeLog.java
@@ -9,94 +9,94 @@ import java.text.*;
  */
 public class SnakeLog
 {
-  private SimpleDateFormat df;
-  private String fileName = "snake.log";
-  private FileWriter writer = null;
+	private SimpleDateFormat df;
+	private String fileName = "snake.log";
+	private FileWriter writer = null;
 
-  /**
-  * Default constructor for a newly created snake log object
-  */
-  public SnakeLog()
-  {
+	/**
+	 * Default constructor for a newly created snake log object
+	 */
+	public SnakeLog()
+	{
 
-    df = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
-    try
-     {
-       writer = new FileWriter(fileName,true);
-       writeLogEntry("----- Start Logging -----");
-     }
-     catch (IOException ex)
-    {
-      System.err.println("Error: Can't create log file");
-      writer = null;
-    }
-  }
+		df = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss" );
+		try
+		{
+			writer = new FileWriter(fileName,true);
+			writeLogEntry("----- Start Logging -----");
+		}
+		catch (IOException ex)
+		{
+			System.err.println("Error: Can't create log file");
+			writer = null;
+		}
+	}
 
-  protected void finalize() throws Throwable
-  {
-    close();
-  }
+	protected void finalize() throws Throwable
+	{
+		close();
+	}
 
-  /**
-   * Write log entry with timestamp to log
-   *
-   * @param info String
-   */
-  public void writeLogEntry(String info)
-  {
-    if (writer != null)
-    {
-      java.util.Calendar cal = java.util.Calendar.getInstance();
-      try
-      {
-        writer.write(df.format(cal.getTime()) + ": " + info + "\r\n");
-        writer.flush();
-      }
-      catch (IOException ex)
-     {
-       System.err.println("Error: Can't write to log file");
-     }
-    }
-  }
+	/**
+	 * Write log entry with timestamp to log
+	 *
+	 * @param info String
+	 */
+	public void writeLogEntry(String info)
+	{
+		if (writer != null)
+		{
+			java.util.Calendar cal = java.util.Calendar.getInstance();
+			try
+			{
+				writer.write(df.format(cal.getTime()) + ": " + info + "\r\n");
+				writer.flush();
+			}
+			catch (IOException ex)
+			{
+				System.err.println("Error: Can't write to log file");
+			}
+		}
+	}
 
-  /**
-   * Close the log file
-   */
-  public void close()
-  {
-    if (writer != null)
-    {
-      try
-      {
-        writeLogEntry("----- Stop Logging -----");
-        writer.flush();
+	/**
+	 * Close the log file
+	 */
+	public void close()
+	{
+		if (writer != null)
+		{
+			try
+			{
+				writeLogEntry("----- Stop Logging -----");
+				writer.flush();
 
-        writer.close();
-      }
-      catch (IOException ex)
-      {
-        System.err.println("Error: Can't close log file");
-      }
-      writer = null;
-    }
-  }
+				writer.close();
+			}
+			catch (IOException ex)
+			{
+				System.err.println("Error: Can't close log file");
+			}
+			writer = null;
+		}
+	}
 
-  /**
-  * Write all buffed data into to file
-  */
- public void flush()
- {
-   if (writer != null)
-   {
-     try
-     {
-       writer.flush();
-     }
-     catch (IOException ex)
-     {
-       System.err.println("Error: Can't flush log file");
-     }
-   }
- }
+	/**
+	 * Write all buffed data into to file
+	 */
+	public void flush()
+	{
+		if (writer != null)
+		{
+			try
+			{
+				writer.flush();
+			}
+			catch (IOException ex)
+			{
+				System.err.println("Error: Can't flush log file");
+			}
+		}
+	}
 
 }
